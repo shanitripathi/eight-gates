f3e1d3ffa9909485b0459a3ce6f39cbdb8eff125
shani__tripathi <shani.tripathi01@gmail.com>
2021-01-08 00:33:34 +0000

    mobile frielndly

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -123,7 +123,7 @@ const GameDetail = ({ id }) => {
 const CardShadow = styled(motion.div)`
   width: 100%;
   min-height: 100vh;
-  overflow-y: scroll;
+  overflow-y: hidden;
   background-color: rgba(0, 0, 0, 0.5);
   position: fixed;
   top: 0;
